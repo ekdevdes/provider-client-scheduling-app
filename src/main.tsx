a2eee25673e37055198d9cc4503a9e7348a733e3
Ethan Kramer <ethankr234@gmail.com>
2024-05-19 15:42:26 -0400

    chore: addl comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
+// Libs
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux';
 import store from './store';
 
+// Internl components
 import App from './components/App'
 
+// Normalize helps ensure all browsers are starting from the same baseline styles (since they can be a little different)
 import './normalize.css'
-import './index.scss'
 
 // In a production app I'd add an ErrorBoundary component here to show a pretty error screen to the user and record the js error to datadog
 ReactDOM.createRoot(document.getElementById('root')!).render(
